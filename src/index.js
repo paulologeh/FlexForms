171d1695954427425f9f5aab0ef1418fc9f73714
paulologeh <ologehmamezi@gmail.com>
2020-07-08 21:14:04 +0100

    trying to get gh

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 import PanelApp from './PanelApp';
 import * as serviceWorker from './serviceWorker';
 
-render(
+ReactDOM.render(
     <React.StrictMode>
         <PanelApp />
     </React.StrictMode>,
